87ffcfe617abaa0cf596469013ba803f3efa6a5d
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-01-15 00:45:06 +0100

    Upgrade deps; FIX for bootstrap-vue is not needed now

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,17 +5,7 @@ import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 
-// FIXME https://github.com/bootstrap-vue/bootstrap-vue/issues/1201
-let originalVueComponent = Vue.component
-Vue.component = function (name, definition) {
-  if (name === 'bFormCheckboxGroup' || name === 'bCheckboxGroup' ||
-      name === 'bCheckGroup' || name === 'bFormRadioGroup') {
-    definition.components = {bFormCheckbox: definition.components[0]}
-  }
-  originalVueComponent.apply(this, [name, definition])
-}
 Vue.use(BootstrapVue)
-Vue.component = originalVueComponent
 
 Vue.config.productionTip = false
 
