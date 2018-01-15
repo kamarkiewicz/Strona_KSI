dee2cc632b0be8bf41b9da8117a245a511799b52
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-01-15 02:00:26 +0100

    Reorder CSS imports according to docs [ci skip]

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -82,8 +82,8 @@
 </template>
 
 <script>
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'font-awesome/css/font-awesome.css'
 
 export default {
