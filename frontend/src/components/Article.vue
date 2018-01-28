fe767aa307fbc26bb29720df83a1b8abca9be3dd
SzyKo <kopaszymon97@gmail.com>
2018-01-28 14:59:36 +0100

    Basic vue script in ArticleComponent

--- frontend/src/components/Article.vue
+++ frontend/src/components/Article.vue
@@ -1,10 +1,24 @@
 <template>
-
+  <div class="container mt-5">
+    
+  </div>
 </template>
 
 <script>
+export default {
+  name: 'ArticleComponent',
+  data () {
+    return {
+      title: 'Nazwa artykułu',
+      content: 'Treść artykułu',
+      image: 'Źródło zdjęcia głównego'
+        
+    }
+  }
+}
 </script>
 
 <style lang="scss">
+
 </style>
 
