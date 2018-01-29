59e4097c16bdbcc875c9f7fc1fc2617dca2d7be8
SzyKo <szyko@kopciuch.mmp.pl>
2018-01-29 21:39:35 +0100

    Add article component to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,14 @@ import Router from 'vue-router'
 
 import HomeComponent from '@/components/Home'
 import GalleryComponent from '@/components/Gallery'
+import ArticleComponent from '@/components/Article'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/gallery', name: 'GalleryComponent', component: GalleryComponent },
-    { path: '/', name: 'HomeComponent', component: HomeComponent }
+    { path: '/', name: 'HomeComponent', component: HomeComponent },
+    { path: '/article', name: 'ArticleComponent', component: ArticleComponent }
   ]
 })
