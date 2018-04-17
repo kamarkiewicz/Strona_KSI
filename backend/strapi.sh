e10befb9f8a6eba37d3f33dfb5f708f32a191240
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-04-17 21:54:04 +0200

    fixups

--- backend/strapi.sh
+++ backend/strapi.sh
@@ -19,7 +19,12 @@ DATABASE_NAME=${DATABASE_NAME:-strapi}
 
 if [ ! -f "$APP_NAME/package.json" ]
 then
-    strapi new ${APP_NAME} --dbclient=$DATABASE_CLIENT --dbhost=$DATABASE_HOST --dbport=$DATABASE_PORT --dbname=$DATABASE_NAME --dbusername=$DATABASE_USERNAME --dbpassword=$DATABASE_PASSWORD
+    strapi new ${APP_NAME} --dbclient=$DATABASE_CLIENT \
+                           --dbhost=$DATABASE_HOST \
+                           --dbport=$DATABASE_PORT \
+                           --dbname=$DATABASE_NAME \
+                           --dbusername=$DATABASE_USERNAME \
+                           --dbpassword=$DATABASE_PASSWORD
 fi
 
 cd $APP_NAME
