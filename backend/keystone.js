61889ae2698f01220fe0d0259ea7f254af0ea0c2
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-04-18 22:50:01 +0200

    Do porównania bez fancy elementów

--- backend/keystone.js
+++ backend/keystone.js
@@ -19,8 +19,6 @@ keystone.init({
 	'views': 'templates/views',
 	'view engine': 'pug',
 
-	'emails': 'templates/emails',
-
 	'auto update': true,
 	'session': true,
 	'auth': true,
@@ -47,23 +45,11 @@ keystone.set('routes', require('./routes'));
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
 	posts: ['posts', 'post-categories'],
-	galleries: 'galleries',
-	enquiries: 'enquiries',
 	users: 'users',
 });
 
 // Start Keystone to connect to your database and initialise the web server
 
-
-if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
-	console.log('----------------------------------------'
-	+ '\nWARNING: MISSING MAILGUN CREDENTIALS'
-	+ '\n----------------------------------------'
-	+ '\nYou have opted into email sending but have not provided'
-	+ '\nmailgun credentials. Attempts to send will fail.'
-	+ '\n\nCreate a mailgun account and add the credentials to the .env file to'
-	+ '\nset up your mailgun integration');
-}
 
 
 keystone.start();
