21ed1b844208aa213d0420db58def1201c03a21e
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-04-26 14:18:51 +0200

    Fix: change locale on enter the URL

--- frontend/plugins/i18n.js
+++ frontend/plugins/i18n.js
@@ -1,4 +1,6 @@
 export default function ({ app }) {
+  app.store.commit('SET_LOCALE', app.i18n.locale)
+
   app.i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {
     app.store.commit('SET_LOCALE', newLocale)
   }
