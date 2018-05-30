28c53c61fb5250eb130c8e516e8216f1c22a84b9
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-05-30 10:58:12 +0200

    HomeNews: populate unit tests with mocked data

--- frontend/test/unit/specs/HomeNews.spec.js
+++ frontend/test/unit/specs/HomeNews.spec.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import { mount } from '@vue/test-utils'
 import HomeNews from '~/components/HomeNews.vue'
+import { Image } from '~/assets/js/utils'
 
 Vue.config.ignoredElements = [
   'b-row', 'b-col', 'nuxt-link'
@@ -14,6 +15,34 @@ describe('HomeNews', () => {
       mocks: {
         $t: (label) => { return label },
         $d: (label) => { return label },
+      },
+      propsData: {
+        news: [
+          {
+            id: 1,
+            date: 1527628516335,
+            title: 'Tytuł aktualności 1',
+            excerpt: '<p>A party you cannot refuse...</p><p>Osoby zainteresowane proszone są o imienny zapis w pokoju KSI.</p>',
+            image: new Image('https://picsum.photos/270/180?image=1024'),
+            link: '#read-more-link'
+          },
+          {
+            id: 2,
+            date: 1527628516335,
+            title: 'Tytuł aktualności 2',
+            excerpt: '<p>A party you cannot refuse...</p><p>Osoby zainteresowane proszone są o imienny zapis w pokoju KSI.</p>',
+            image: new Image('https://picsum.photos/270/180?image=1020'),
+            link: '#read-more-link'
+          },
+          {
+            id: 3,
+            date: 1527628516335,
+            title: 'Tytuł aktualności 3',
+            excerpt: '<p>A party you cannot refuse...</p><p>Osoby zainteresowane proszone są o imienny zapis w pokoju KSI.</p>',
+            image: new Image('https://picsum.photos/270/180?image=1010'),
+            link: '#read-more-link'
+          },
+        ],
       },
     })
   })
