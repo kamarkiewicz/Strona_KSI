59ebd95950b3cb6a98a4fa227e16ae83243949ca
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-05-30 03:31:17 +0200

    Fix and refactor tests

--- frontend/test/unit/specs/HomeNews.spec.js
+++ frontend/test/unit/specs/HomeNews.spec.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
-import { shallowMount, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HomeNews from '~/components/HomeNews.vue'
 
 Vue.config.ignoredElements = [
-  'b-row', 'b-col'
+  'b-row', 'b-col', 'nuxt-link'
 ];
 
-const cfg = {
-  mocks: {
-    $t: (label) => { return label },
-    $d: (label) => { return label },
-  },
-}
-
 describe('HomeNews', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(HomeNews, {
+      mocks: {
+        $t: (label) => { return label },
+        $d: (label) => { return label },
+      },
+    })
+  })
+
   it('renders correctly', () => {
-    const wrapper = mount(HomeNews, cfg)
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('contains News header', () => {
     const label = 'general.news'
-    const wrapper = shallowMount(HomeNews, cfg)
     expect(wrapper.text()).toMatch(label)
   })
 })
