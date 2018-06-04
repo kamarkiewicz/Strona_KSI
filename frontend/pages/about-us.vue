9ee465ca6dd2b96c356bbbcffd6258ee19eb314d
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-04 21:45:28 +0200

    Fix on dynamically changing footer padding-top; TODO find more consistent way to make a stripe

--- frontend/pages/about-us.vue
+++ frontend/pages/about-us.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="history-page outer">
+  <section class="history-page bg-light">
     <div class="container pt-5">
 
       <header class="major">
@@ -20,46 +20,25 @@ export default {
   components: {
     Timeline
   },
-  data() {
+  data () {
     return {
       subtitle: "Quibusdam dicentibus",
       title: "Alii sementem faciunt, alii metunt"
+    }
+  },
+  head () {
+    return {
+      title: 'O nas',
     }
   }
 }
 </script>
 
-<style lang="scss">
-@import '~assets/scss/utils';
+<style lang="scss" scoped>
 
-$bg-color: #f5f8fa;
 $subheadtitle-color: #00a6d9;
-$content-color: #637485;
-
-.footer {
-  padding-top: 7em;
-}
 
 .history-page {
-  &.outer {
-    background-color: $bg-color;
-    color: $content-color;
-    position: relative;
-
-    &::after {
-      content: '';
-      position: absolute;
-      left: 0;
-      bottom: 0;
-
-      width: 100%;
-      height: 5em;
-
-      @include bg-triangle($bg-color);
-      transform: translateY(100%) rotate(180deg);
-    }
-  }
-
   header.major {
     p {
       color: $subheadtitle-color;
