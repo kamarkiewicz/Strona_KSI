5e25df8f27736b6f50ed489f05f6b13b218fcd83
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-07 00:08:22 +0200

    Fix HEAD title

--- frontend/pages/album/_gallery.vue
+++ frontend/pages/album/_gallery.vue
@@ -42,7 +42,7 @@ export default {
   },
   head () {
     return {
-      title: this.title,
+      title: this.entry.title,
     }
   }
 }
--- frontend/pages/news/_article.vue
+++ frontend/pages/news/_article.vue
@@ -43,7 +43,7 @@ export default {
   },
   head () {
     return {
-      title: this.title,
+      title: this.entry.title,
     }
   }
 }
