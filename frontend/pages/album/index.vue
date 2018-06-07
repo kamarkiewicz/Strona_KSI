3142d4537bd69ca84b5371c0a724e6c481d2bb75
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-07 19:23:00 +0200

    Album page: header

--- frontend/pages/album/index.vue
+++ frontend/pages/album/index.vue
@@ -1,23 +1,25 @@
 <template>
-  <section class="album-page bg-light">
+  <section class="album-page py-5 bg-light">
 
-    <div class="container pt-5 pb-4">
-      <h2 class="text-primary pb-4">Album galerii</h2>
-      <p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
+    <div class="container">
+      <header>
+        <h2 class="text-primary pb-4">Album galerii</h2>
+        <p class="text-secondary">Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
+      </header>
     </div>
 
-    <section class="container px-5">
-      <b-row>
-        <b-col md="12" lg="6" class="p-3" v-for="el in galleries" :key="el.id">
+    <div class="container px-5">
+      <div class="row">
+        <div class="col-md-12 col-lg-6 p-3" v-for="el in galleries" :key="el.id">
           <preview-card
             :image="el.image"
             :title="el.title"
             :excerpt="el.description"
             :link="localePath({ name: 'album-gallery', params: { gallery: el.link }})"
             linkText="Przejdź do galerii" />
-        </b-col>
-      </b-row>
-    </section>
+        </div>
+      </div>
+    </div>
 
   </section>
 </template>
