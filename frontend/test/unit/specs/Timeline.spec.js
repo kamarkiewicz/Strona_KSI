7de6a732a4d688dfb2e45df87205869c3e8a0c19
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-07 15:45:26 +0200

    Update test

--- frontend/test/unit/specs/Timeline.spec.js
+++ frontend/test/unit/specs/Timeline.spec.js
@@ -14,25 +14,25 @@ describe('Timeline', () => {
       propsData: {
         entries: [
           {
-            id: 1,
-            date: "09.04.16",
-            title: "Magna etiam sed lorem ipsum dolor amet",
-            image: "https://picsum.photos/g/300/180?image=81",
-            content: "Phasellus in ante vitae nibh porttitor tempus vitae ut ante. Vestibulum blandit dolor elementum viverra. Sed quat a diam, aliquet tempus felis. Phasellus et magna vitae nibh porttitor tempus vitae.",
-          },
-          {
-            id: 2,
-            date: "08.28.16",
+            id: "5b16e3ae076badoc882781130",
+            date: "2017-08-10",
             title: "Sed feugiat et mauris adipiscing dolor",
-            image: "https://picsum.photos/g/300/180?image=82",
-            content: "Phasellus in ante vitae nibh porttitor tempus vitae ut ante. Vestibulum blandit dolor elementum viverra. Sed quat a diam, aliquet tempus felis. Phasellus et magna vitae nibh porttitor tempus vitae.",
+            image: new Image('https://picsum.photos/g/264/134?image=82'),
+            content: "<p>Phasellus in ante vitae nibh porttitor tempus vitae ut ante. Vestibulum blandit dolor elementum viverra. Sed quat a diam, aliquet tempus felis. Phasellus et magna vitae nibh porttitor tempus vitae.</p>"
           },
           {
-            id: 3,
-            date: "08.22.16",
+            id: "5b16e3ae0777adoc182324768",
+            date: "2016-04-09",
+            title: "Magna etiam sed lorem ipsum dolor amet",
+            image: new Image('https://picsum.photos/g/264/134?image=81'),
+            content: "<p>Phasellus in ante vitae nibh porttitor tempus vitae ut ante. Vestibulum blandit dolor elementum viverra. Sed quat a diam, aliquet tempus felis. Phasellus et magna vitae nibh porttitor tempus vitae.</p>"
+          },
+          {
+            id: "5b16e3ae077e9doc180358326",
+            date: "2008-10-14",
             title: "Tellus magna dolore sed elementum",
-            image: "https://picsum.photos/g/300/180?image=83",
-            content: "Phasellus in ante vitae nibh porttitor tempus vitae ut ante. Vestibulum blandit dolor elementum viverra. Sed quat a diam, aliquet tempus felis. Phasellus et magna vitae nibh porttitor tempus vitae.",
+            image: new Image('https://picsum.photos/g/264/134?image=83'),
+            content: "<p>Phasellus in ante vitae nibh porttitor tempus vitae ut ante. Vestibulum blandit dolor elementum viverra. Sed quat a diam, aliquet tempus felis. Phasellus et magna vitae nibh porttitor tempus vitae.</p>"
           },
         ],
       },
