a3051e9e92c5205c23d0d02e94f82c823fee03d2
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-07 17:27:00 +0200

    HomeNews: cleanup styles (looks same)

--- frontend/components/HomeNews.vue
+++ frontend/components/HomeNews.vue
@@ -1,13 +1,13 @@
 <template>
-  <section class="news wrapper pb-3">
+  <section class="home-news pb-3 bg-light">
 
-    <div class="container pt-5">
-      <h2 class="major">{{ $t('general.news') }}</h2>
+    <div class="container pt-5 pb-4">
+      <h2 class="text-primary" v-text="$t('general.news')"></h2>
     </div>
 
     <section class="container px-5 pb-5">
-      <b-row>
-        <b-col lg="4" class="pr-3 pb-5" v-for="el in news" :key="el.id">
+      <div class="row">
+        <div class="col-lg-4 pr-3 pb-5" v-for="el in news" :key="el.id">
           <preview-card
             :image="el.image"
             :date="el.date"
@@ -15,8 +15,8 @@
             :excerpt="el.excerpt"
             :link="localePath({ name: 'news-article', params: { article: el.link }})"
             linkText="Czytaj więcej" />
-        </b-col>
-      </b-row>
+        </div>
+      </div>
     </section>
 
   </section>
@@ -32,39 +32,9 @@ export default {
   },
   props: {
     news: {
-      type: Array,
+      type: Array, // must contain max 3 entries
       required: true
     },
   },
 }
 </script>
-
-<style lang="scss" scoped>
-
-$bg-color: #f5f8fa;
-$headtitle-color: #00a6d9;
-$content-color: #637485;
-
-.news {
-  &.wrapper {
-      background-color: $bg-color;
-      color: $content-color;
-      position: relative;
-  }
-
-  h1.major, h3.major, h4.major, h5.major, h6.major {
-      border-bottom: solid 2px rgba(255, 255, 255, 0.125);
-  }
-
-  h3 {
-      font-size: 1.2em;
-  }
-
-  h1, h2, h3, h4, h5, h6 {
-      color: $headtitle-color;
-      font-weight: 400;
-      margin: 0 0 1em 0;
-  }
-}
-
-</style>
