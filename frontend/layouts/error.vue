0b244740da14c87221ecc655e2c27c6e355a3880
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-08 11:59:11 +0200

    Handle errors on pages with slugs

--- frontend/layouts/error.vue
+++ frontend/layouts/error.vue
@@ -2,7 +2,7 @@
   <div class="error-page container h-100">
     <div class="row h-100">
       <div class="col-12 text-center vcenter">
-        <h1 v-if="error.statusCode === 404">This page could not be found</h1>
+        <h1 v-if="error.statusCode === 404">{{ error.message || 'This page could not be found' }}</h1>
         <h1 v-else>An error occurred</h1>
         <nuxt-link to="/">Back to the home page</nuxt-link>
       </div>
--- frontend/pages/album/_gallery.vue
+++ frontend/pages/album/_gallery.vue
@@ -23,12 +23,20 @@ export default {
       entry: 'album/entry',
     })
   },
-  async fetch ({ app, params, store }) {
+  async fetch ({ app, params, store, error }) {
     const ctx = { axios: app.$axios, slug: params.gallery }
-    await Promise.all([
-      store.dispatch('album/getSingle', ctx),
-      store.dispatch('album/getLocalSlugs', ctx)
-    ])
+    try {
+      await Promise.all([
+        store.dispatch('album/getSingle', ctx),
+        store.dispatch('album/getLocalSlugs', ctx)
+      ])
+    } catch (err) {
+      if (err.name === 'EntryNotFound') {
+        error({ statusCode: 404, message: 'There is no such entry' })
+      } else {
+        error({ statusCode: 500, message: err.message })
+      }
+    }
   },
   mounted () {
     const that = this;
