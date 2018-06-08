b3750f0f8993f2db164698c26d48b0d06427b184
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-08 11:04:27 +0200

    Errors should be handled by utils' function callers

--- frontend/assets/js/utils.js
+++ frontend/assets/js/utils.js
@@ -59,17 +59,11 @@ export async function fetchSlugsByTitle (axios, collectionName, { locale, slug }
 
 // Gets region data through API
 export async function fetchRegion (axios, region, locale) {
-  let data
-  try {
-    data = await axios.$get(`/api/regions/data/${region}`, {
-      params: {
-        lang: locale
-      }
-    })
-  }
-  catch (e) {
-    console.error(e)
-  }
+  let data = await axios.$get(`/api/regions/data/${region}`, {
+    params: {
+      lang: locale
+    }
+  })
   return data
 }
 
@@ -89,17 +83,11 @@ export async function fetchSlides (axios, region, locale) {
 
 // Gets collection data through API
 export async function fetchCollection (axios, collectionName, locale) {
-  let data
-  try {
-    data = await axios.$get(`/api/collections/get/${collectionName}`, {
-      params: {
-        lang: locale
-      }
-    })
-  }
-  catch (e) {
-    console.error(e)
-  }
+  let data = await axios.$get(`/api/collections/get/${collectionName}`, {
+    params: {
+      lang: locale
+    }
+  })
   return data
 }
 
@@ -107,20 +95,13 @@ export async function fetchCollection (axios, collectionName, locale) {
 export async function fetchSingleByTitle (axios, collectionName, slug, locale) {
   const field = locale !== BACK_DEFAULT_LOCALE ? `title_${locale}_slug` : 'title_slug'
   const filter = `filter[${field}]`
-  let data
-  try {
-    const { entries, total } = await axios.$get('/api/collections/get/' + collectionName, {
-      params: {
-        [filter]: slug,
-        lang: locale
-      }
-    })
-    if (total > 1) console.error(`There is ${total} entries with slug ${slug} in ${locale}`)
-    if (!entries.length) throw new Error('There is no such entry')
-    data = entries[0]
-  }
-  catch (e) {
-    console.error(e)
-  }
-  return data
+  const { entries, total } = await axios.$get('/api/collections/get/' + collectionName, {
+    params: {
+      [filter]: slug,
+      lang: locale
+    }
+  })
+  if (total > 1) console.error(`There is ${total} entries with slug ${slug} in ${locale}`)
+  if (!entries.length) throw new Error('There is no such entry')
+  return entries[0]
 }
