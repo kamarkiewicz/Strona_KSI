5f9aacf7fbf767656aae8aee72cb82fca7120430
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-10 11:22:05 +0200

    Pages Album and News should look same, especially on mobile devices

--- frontend/pages/album/index.vue
+++ frontend/pages/album/index.vue
@@ -1,16 +1,16 @@
 <template>
   <section class="album-page pt-5 pb-slant bg-light">
 
-    <div class="container">
+    <div class="container pb-4">
       <header>
         <h2 class="text-primary pb-4">Album galerii</h2>
         <p class="text-secondary">Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
       </header>
     </div>
 
-    <div class="container px-5 pb-4">
-      <div class="row equal">
-        <div class="col-md-12 col-lg-6 p-3" v-for="el in galleries" :key="el.id">
+    <section class="container">
+      <div class="row equal" v-for="(chunk, index) in entryChunks" :key="`chunk-${index}`">
+        <div class="col-md-12 col-lg-6 p-3" v-for="el in chunk" :key="el.id">
           <preview-card
             :image="el.image"
             :title="el.title"
@@ -19,13 +19,14 @@
             linkText="Przejdź do galerii" />
         </div>
       </div>
-    </div>
+    </section>
 
   </section>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import _ from 'lodash'
 import PreviewCard from "~/components/PreviewCard.vue"
 
 export default {
@@ -34,9 +35,12 @@ export default {
   },
   computed: {
     ...mapGetters({
-      galleries: 'album/entries',
+      entries: 'album/entries',
       totalRows: 'album/entriesCount',
-    })
+    }),
+    entryChunks () {
+      return _.chunk(this.entries, 2)
+    },
   },
   async fetch ({ app, store }) {
     await store.dispatch('album/getEntries', { axios: app.$axios })
