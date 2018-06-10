84c6262778321a2171e45e86c65d6a9a2f41df9f
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-10 10:15:16 +0200

    Use vue-gallery as a pretty slider for gallery
    CSS Styles
    
    Lazy loading
    
    Fix index
    
    sth
    
    fa

--- frontend/pages/album/_gallery.vue
+++ frontend/pages/album/_gallery.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="container mt-5 pb-slant">
+  <div class="container mt-5 pb-slant hmin-stronaksi">
     <h4>{{ entry.album }}</h4>
     <h1>{{ entry.title }}</h1>
     <div class="gallery-container mt-4 mb-5">
-      <a v-for="img in entry.images" :key="img.title" :href="img.src">
-        <b-img class="img-fluid"
-               :src="img.src"
-               :width="img.width"
-               :height="img.height"
-               :alt="img.title" />
+      <a v-for="(img, imgIndex) in images" :key="img.title"
+         :href="img.src" @click.prevent="index = imgIndex">
+        <b-img-lazy
+          class="py-2 px-0" fluid
+          :src="img.src"
+          :width="img.width"
+          :height="img.height"
+          :alt="img.title" />
       </a>
     </div>
+
+    <vue-gallery
+      :images="imageLinks"
+      :index="index"
+      @close="index = null"
+    ></vue-gallery>
   </div>
 </template>
 
@@ -18,10 +26,25 @@
 import { mapGetters } from 'vuex'
 
 export default {
+  data () {
+    return {
+      index: null,
+    }
+  },
   computed: {
     ...mapGetters({
       entry: 'album/entry',
-    })
+    }),
+    images () {
+      return this.entry.images
+    },
+    imageLinks () {
+      return this.images.map(img => ({
+        title: img.title,
+        description: '',
+        href: img.src,
+      }))
+    }
   },
   fetch ({ app, params, store, error }) {
     const ctx = { axios: app.$axios, slug: params.gallery }
@@ -57,12 +80,6 @@ export default {
 
 <style lang="scss">
 .gallery-container {
-  min-height: 100vh;
-
-  img {
-    padding: 8px 0;
-  }
-
   column-count: 1;
 
   @media (min-width: 720px) { // md
