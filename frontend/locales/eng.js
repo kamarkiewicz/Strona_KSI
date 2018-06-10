27e5dde2ae83165f93f6babdb8895e4e37ebf8ae
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-10 16:51:11 +0200

    Translate AppFooter

--- frontend/locales/eng.js
+++ frontend/locales/eng.js
@@ -1,10 +1,20 @@
 export const general = {
   news: 'News',
+  aboutUs: 'About us',
+  projects: 'Projects',
+  members: 'Members',
   album: 'Galleries',
+  contact: 'Contact',
 };
 
 export const homecarousel = {
   title: 'See for yourself!',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   gotoAlbum: 'Go to the gallery',
+};
+
+export const appfooter = {
+  leftSectionCaption: 'Other links',
+  privacyCaption: 'Privacy',
+  adminPanelCaption: 'Admin panel',
 };
--- frontend/locales/pol.js
+++ frontend/locales/pol.js
@@ -1,10 +1,20 @@
 export const general = {
   news: 'Aktualności',
-  album: 'Album galerii'
+  aboutUs: 'O nas',
+  projects: 'Projekty',
+  members: 'Członkowie',
+  album: 'Album galerii',
+  contact: 'Kontakt',
 };
 
 export const homecarousel = {
   title: 'Zobacz na własne oczy!',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   gotoAlbum: 'Przejdź do galerii',
+};
+
+export const appfooter = {
+  leftSectionCaption: 'Inne linki',
+  privacyCaption: 'Polityka prywatności',
+  adminPanelCaption: 'Panel administracyjny',
 };
