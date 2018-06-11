7917ad2ef17f12b9c7ed730a7771b699e588a44a
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-11 17:40:42 +0200

    Refactor: use mapGetters

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home-page">
 
-    <home-news class="pb-slant" :news="news" />
+    <home-news class="pb-slant" :news="news.slice(0, 3)" />
 
-    <showcase class="slant-in-both" :entries="entries" />
+    <showcase class="slant-in-both" :entries="homepage.showcase" />
 
-    <home-carousel class="pt-slant pb-slant" :slides="slides" />
+    <home-carousel class="pt-slant pb-slant" :slides="homepage.slides" />
 
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import HomeNews from '~/components/HomeNews.vue'
 import Showcase from '~/components/Showcase.vue'
 import HomeCarousel from '~/components/HomeCarousel.vue'
@@ -22,15 +23,10 @@ export default {
     HomeCarousel
   },
   computed: {
-    news () {
-      return this.$store.getters['news/entries'].slice(0, 3)
-    },
-    entries () {
-      return this.$store.getters['homepage'].showcase
-    },
-    slides () {
-      return this.$store.getters['homepage'].slides
-    },
+    ...mapGetters({
+      news: 'news/entries',
+      homepage: 'homepage',
+    }),
   },
   fetch ({ app, store }) {
     const ctx = { axios: app.$axios }
