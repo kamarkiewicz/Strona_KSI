af37dcbe9da0ec89fea156875e11c2b79ecb3240
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2018-06-12 20:57:05 +0200

    Use lazy loading of images-heavy content

--- frontend/components/Showcase.vue
+++ frontend/components/Showcase.vue
@@ -5,7 +5,7 @@
       <div class="row pt-4" v-for="(el, index) in entries" :key="index">
         <div class="col-md-5 align-self-center"
             :class="{ 'order-md-2': index % 2 === 0 }">
-          <img class="img-fluid d-block mb-4 w-100 img-thumbnail" :src="el.image.src" :alt="el.image.title">
+          <b-img-lazy class="img-fluid d-block mb-4 w-100 img-thumbnail" :src="el.image.src" :alt="el.image.title" />
         </div>
         <div class="col-md-7">
           <h2 class="text-primary pt-3" v-text="el.title"></h2>
